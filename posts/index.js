2fd6177e14c19cf0c36985ed4b8f01d464341aaa
Liz <lizzieaxworthy@gmail.com>
2023-11-06 08:52:28 -0800

    app complete

--- posts/index.js
+++ posts/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const {randomBytes} = require('crypto')
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 const posts = {};
 
 app.get('/posts', (req, res)=>{
+    console.log('Request Received')
     res.send(posts);
 });
 
