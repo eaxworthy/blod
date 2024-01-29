12bf9c51d34b470f38f6270bf7bcf6d727e4f87d
Liz <lizzieaxworthy@gmail.com>
2024-01-28 20:05:17 -0800

    up through video 42

--- front-end/src/PostList.js
+++ front-end/src/PostList.js
@@ -7,7 +7,8 @@ const PostList = () => {
     const [posts, setPosts] = useState({});
 
     const fetchPosts = async () => {
-        const res = await axios.get('http://localhost:4000/posts');
+        const res = await axios.get('http://localhost:4002/posts');
+        console.log(res.data);
         setPosts(res.data);
     };
 
@@ -24,7 +25,7 @@ const PostList = () => {
             <div className='card-body'>
                 <h3>{post.title}</h3>
             </div>
-            <CommentList postId={post.id}/>
+            <CommentList comments={post.comments} />
             <CommentCreate postId={post.id}/>
         </div>
         );
